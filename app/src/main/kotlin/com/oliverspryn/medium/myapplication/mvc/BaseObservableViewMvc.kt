82c98b935141e1e1399f83b5bb66d0729b00571c
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-10-29 16:19:52 -0400

    First unit test

--- app/src/main/kotlin/com/oliverspryn/medium/myapplication/mvc/BaseObservableViewMvc.kt
+++ app/src/main/kotlin/com/oliverspryn/medium/myapplication/mvc/BaseObservableViewMvc.kt
@@ -1,10 +1,12 @@
 package com.oliverspryn.medium.myapplication.mvc
 
-import java.util.ArrayList
+import java.util.*
 
 abstract class BaseObservableViewMvc<Listener> : BaseViewMvc(), ObservableViewMvc<Listener> {
 
     private val listeners = ArrayList<Listener>()
+
+    override fun getListeners(): List<Listener> = Collections.unmodifiableList(listeners)
 
     override fun registerListener(listener: Listener) {
         listeners.add(listener)
--- app/src/main/kotlin/com/oliverspryn/medium/myapplication/mvc/ObservableViewMvc.kt
+++ app/src/main/kotlin/com/oliverspryn/medium/myapplication/mvc/ObservableViewMvc.kt
@@ -1,6 +1,7 @@
 package com.oliverspryn.medium.myapplication.mvc
 
 interface ObservableViewMvc<Listener> : ViewMvc {
+    fun getListeners(): List<Listener>
     fun registerListener(listener: Listener)
     fun unregisterListener(listener: Listener)
 }
