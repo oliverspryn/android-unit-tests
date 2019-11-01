78c99e2f5fa13b95f6f44bc8e50d3c69fee541b9
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-11-01 11:56:34 -0400

    Add Dagger injector implementation

--- app/src/main/java/com/oliverspryn/medium/androidunittests/MainActivity.kt
+++ app/src/main/java/com/oliverspryn/medium/androidunittests/MainActivity.kt
@@ -2,11 +2,13 @@ package com.oliverspryn.medium.androidunittests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oliverspryn.medium.androidunittests.dagger.DaggerInjector
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DaggerInjector.buildMainComponent(this)
         setContentView(R.layout.activity_main)
     }
 }
